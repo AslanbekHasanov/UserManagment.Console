682307c98c75c8bfdf289538154f3790c25032ef
AslanbekHasanov <learneraslanbek1220@gmail.com>
2024-03-13 01:14:10 +0500

    Validate And Check Login

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
 ﻿
 
+using System;
 using UserManagment.Console.Brokers.Storages;
 using UserManagment.Console.Models.Users;
+using UserManagment.Console.Services;
 
-IStorageBroker broker = new FileStorageBroker();
-broker.AddUser(new User() { Name = "Aslanbek", Password = "Aslan1220"});
+ILogInService loginService = new LogInService();
+bool res = loginService.LogInCheck(new User() { Name = "Aslann", Password = "Aslan1220"});
+Console.WriteLine(res);

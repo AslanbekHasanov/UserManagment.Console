a9344f944f4217b057ac5304d39e3b138d0bf627
AslanbekHasanov <learneraslanbek1220@gmail.com>
2024-03-12 18:03:24 +0500

    Implement Logging

---
+++ Brokers/Loggings/LoggingBroker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace UserManagment.Console.Brokers.Loggings
+{
+    internal class LoggingBroker : ILoggingBroker
+    {
+        public void LogInformation(string message)=>
+            System.Console.WriteLine(message);
+
+        public void LogError(string userMessage)=>
+            System.Console.WriteLine(userMessage);
+
+        public void LogError(Exception exaption)
+        {
+            System.Console.ForegroundColor = ConsoleColor.Red;
+            System.Console.WriteLine(exaption.Message);
+        }
+    }
+}
